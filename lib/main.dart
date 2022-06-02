df9d9b7c1d03f577e994838238b4dda52fc0de4e
AnriNonaka <anri.nonaka@axia-works.com>
2022-06-02 12:57:46 +0900

    修正

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
 import 'building/building_layout_screen.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Welcome to Flutter',
+    return const MaterialApp(
+        title: 'Tutorial',
         home: Home()
     );
   }
 }
 
 class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,12 +26,12 @@ class Home extends StatelessWidget {
       ),
       body: Center(
         child: ElevatedButton(
-          child: Text('Tutorial1'),
+          child: const Text('Tutorial1'),
           onPressed: () {
             Navigator.push(
               context,
               MaterialPageRoute(
-                builder: (context) => BuildLayoutScreen(),
+                builder: (context) => const BuildLayoutScreen(),
               ),
             );
           },
