8bb5f8aba791d6de46f4b82065ace3cff9f90c3e
AnriNonaka <anri.nonaka@axia-works.com>
2022-06-10 16:51:06 +0900

    Tutorial2-3の実装

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_tutorial/residence/residence_screen.dart';
 import 'package:flutter_tutorial/youtube/youtube_screen.dart';
 import 'animation/animation_screen.dart';
 import 'building/building_layout_screen.dart';
+import 'merukari/merukari_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -72,6 +73,17 @@ class Home extends StatelessWidget {
                     context,
                     MaterialPageRoute(
                       builder: (context) => const ResidenceScreen(),
+                    ),
+                  );
+                },
+              ),
+              ElevatedButton(
+                child: const Text('Tutorial2-3'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const MerukariScreen(),
                     ),
                   );
                 },
